dc89030688dcf9156dc61a23dac61f9fd55e62eb
Joe Fleming <joe.fleming@gmail.com>
2017-03-25 21:02:08 -0700

    update login form styles

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,132 +1,116 @@
 <template>
   <div class="login">
     <logo-brand class="login-logo-brand"></logo-brand>
-    
-    <form
-        id="login-form"
-        v-on:submit.prevent="handleSubmit">
-      <div class="login-title">Dashboard Sign In</div>
-      
-      <md-input-container 
-          v-bind:class="{'md-input-error':emailError && (!email || !email.length)}">
+
+    <form id="login-form"v-on:submit.prevent="handleSubmit">
+      <h2>Dashboard Sign In</h2>
+
+      <md-input-container :class="{ 'md-input-invalid': emailError }">
         <label>Email</label>
-        <md-input
-            type="email"
-            v-model="email"/>
+        <md-input type="email" v-model="email"></md-input>
       </md-input-container>
-      
-      <md-input-container
-          v-bind:class="{'md-input-error':passwordError && (!password || !password.length)}">
+
+      <md-input-container :class="{'md-input-invalid': passwordError }">
         <label>Password</label>
-        <md-input
-            type="password"
-            v-model="password"
-            v-on:keyup.enter="handleSubmit"/>
+        <md-input type="password" v-model="password"></md-input>
       </md-input-container>
-      
+
       <div class="input-row">
-        <a
-            class="forgot-password"
-            href="#"
-            v-on:click.prevent="handleForgotPassword">
+        <!-- <a class="forgot-password" href="#" @click.prevent="handleForgotPassword">
           Forgot Password
-        </a>
-        
-        <button type="submit">
+        </a> -->
+
+        <md-button class="md-raised md-primary" type="submit">
           Submit
-        </button>
+        </md-button>
       </div>
     </form>
   </div>
-  
+
 </template>
 
 <script>
   import LogoBrand from '../components/LogoBrand.vue';
-  
+
   export default {
     name: 'login-form',
-    
+
     components: {
       LogoBrand,
     },
-    
+
     props: {
       onSubmit: Function,
     },
-    
+
     data() {
       return {
-        email: this.email,
-        password: this.password,
+        email: '',
+        password: '',
         emailError: false,
         passwordError: false,
       };
     },
-    
-    computed: {
-      emailClass() {
-        this.emailError = !(this.email && this.email.length);
-      },
-      passwordClass() {
-        this.passwordError = !(this.password && this.password.length);
-      }
-    },
-    
+
     methods: {
-      handleForgotPassword() {
-        // TODO - write forgot password function
+      resetErrors() {
+        this.emailError = false;
+        this.passwordError = false;
       },
       handleSubmit() {
-        if(!this.email || this.email.length < 1) {
-          this.$el.querySelector('input[type=email]').focus();
-          return this.emailError = true;
+        let hasErrors = false;
+        this.resetErrors();
+
+        if (!this.email || this.email.length < 1) {
+          this.emailError = true;
+          hasErrors = true;
         }
-        if(!this.password || this.password.length < 1) {
-          this.$el.querySelector('input[type=password]').focus()
-          return this.passwordError = true;
+
+        if (!this.password || this.password.length < 1) {
+          this.passwordError = true;
+          hasErrors = true;
         }
-        // TODO - write form submission function 
-        
+
+        if (hasErrors) return;
+
         const { email, password } = this;
         this.onSubmit({ email, password });
-      }
-    }
-    
+      },
+    },
   };
 </script>
 
-<style lang="scss">
-body, body.md-theme-default {
-  background-color: #f9f9f9 !important;
-  font-family: 'Roboto';
-}
 
+<style lang="scss">
 .login {
   background-color: #f9f9f9;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  
-  &-logo-brand {
+
+  h2 {
+    text-align: center;
+  }
+
+  .login-logo-brand {
     margin: 0 auto;
     height: 200px;
     width: 100%;
     max-height: 200px;
     max-width:300px;
-    
+
     path {
       fill: #66a52e;
     }
   }
-  
-  &-title {
+
+  .login-title {
     text-align: center;
     font-size: 32px;
     padding: 20px;
     line-height: initial;
   }
-  
+
   form {
     margin: 0 auto;
     background-color: white;
@@ -136,25 +120,20 @@ body, body.md-theme-default {
     border-radius: 6px;
     border: 1px solid #cccccc;
   }
-  
-  .forgot-password {
-    flex: 1;
-  }
-  
-  button[type=submit] {
-    background-color: #66a52e;
-    border: none;
-    border-radius: 6px;
-    color: white;
-    padding: 10px 14px;
-    font-size: 24px;
-  }
-  
+
   .input-row {
     margin-top: 20px;
-    position: relative;
     display: flex;
+    justify-content: flex-end;
     align-items: baseline;
+
+    .forgot-password {
+      flex: 1;
+    }
+
+    button {
+      justify-content: flex-end;
+    }
   }
 }
 </style>
