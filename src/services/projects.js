52d1f34997bdb734e3b9894a43a0406b993fa00a
Bryan Maamo <ninjabryan@gmail.com>
2017-03-26 20:55:06 +0000

    working on projects service

---
+++ src/services/projects.js
@@ -0,0 +1,31 @@
+/* global backand */
+import _ from 'lodash';
+
+const OBJECT_NAME = 'projects';
+
+export default function getAll(filters) {
+  const defaults = {
+    sort: backand.helpers.sort.create('projectStartDate', 'asc'),
+  };
+  const params = _.pick(filters, [
+    'projectName',
+    'projectDescription',
+    'projectStartDate',
+    'projectEndDate',
+    'projectStatus',
+    'totalPositions',
+    'partnerOrganizations',
+    'users',
+    'chapters',
+    'links',
+  ]);
+
+  // force sort to be an object
+  if (typeof params.sort === 'object' && params.sort.name) {
+    params.sort = backand.helpers.sort.create(params.sort.name, params.sort.order || 'asc');
+  } else {
+    delete params.sort;
+  }
+
+  return backand.object.getList(OBJECT_NAME, Object.assign(defaults, params)).then(res => res.data);
+}
