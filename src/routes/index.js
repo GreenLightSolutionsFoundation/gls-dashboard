bdc7ed6d86529b3fe027d50c5e4fdb59654c214a
krish-sai-p <saikrishna.vn930@gmail.com>
2017-03-25 22:17:48 -0700

    Merge errors fixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,10 @@ import login from './login';
 import approvalPending from './approval-pending';
 import demo from './demo';
 import app from './app';
-<<<<<<< HEAD
 import cohort from './cohort';
-=======
 import confidentialityAgreement from './confidentiality-agreement';
 import commitmentAgreement from './commitment-agreement';
 import solutioneering101 from './solutioneering-101';
->>>>>>> 9f074d04404b489d8c4f9521e459ffb8bdfe45cd
 
 const routes = [
   createRoute('/login', login),
@@ -22,13 +19,10 @@ const routes = [
     },
     children: [
       createRoute('', demo),
-<<<<<<< HEAD
       createRoute('/cohort', cohort),
-=======
       createRoute('/confidentiality-agreement', confidentialityAgreement),
       createRoute('/commitment-agreement', commitmentAgreement),
       createRoute('/solutioneering-101', solutioneering101),
->>>>>>> 9f074d04404b489d8c4f9521e459ffb8bdfe45cd
     ],
   }),
 ];
