ef994d751825f362ade4a26a093209dd6421c22b
Bryan Maamo <ninjabryan@gmail.com>
2017-03-31 06:01:51 +0000

    working on looping projects

--- src/pages/ProjectRankingForm.vue
+++ src/pages/ProjectRankingForm.vue
@@ -2,17 +2,41 @@
   <div>
     <h1 class="md-title">Pick your Project!</h1>
     <div class="project-ranking-forms">
-      <project-card v-for="n in 4" project-name="Build Us Hope" partner-name="Singleton Community Services, INC" logo="https://placehold.it/70x70?text=logo" open-positions="4" total-positions="10" project-start-date="2107-11-11" project-end-date="2108-11-11" project-description="description"></project-card>
+      <project-card
+        v-for="p in projects"
+        project-name="p.projectName" 
+        partner-name="p.partnerName" 
+        logo="p.logo" 
+        project-description="p.projectDescription"
+        open-positions="p.openPositions" 
+        total-positions="p.totalPositions"
+        project-start-date="p.projectStartDate"
+        project-end-date="p.projectEndDate">
+      </project-card>
     </div>
   </div>
 </template>
 
 <script>
   import ProjectCard from '../components/ProjectCard.vue';
+  import getAll from '../services/projects';
   
   export default {
     name: 'project-ranking-form',
-    components: {ProjectCard}
+    
+    components: {ProjectCard},
+    
+    data() {
+      return {
+        projects: []
+      }
+    },
+    
+    created() {
+      getAll().then(data => {
+        this.data.projects = data;
+      });
+    }
   };
 </script>
 
