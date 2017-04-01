3a1604d6019b07abfcf936bb8e10995d98b8b0a1
Josh Grauer <jaggedaz@gmail.com>
2017-04-01 13:16:21 -0700

    Finished out the Solutioneering 101 Quiz page.

--- src/pages/Solutioneering101Quiz.vue
+++ src/pages/Solutioneering101Quiz.vue
@@ -1,34 +1,88 @@
 <template>
-  <div>
-    <h1 class="md-title" style="margin-bottom: 40px;">Solutioneering 101 Quiz</h1>
-    <form novalidate @submit.stop.prevent="submit">
-      <div v-for="(question, questionIndex) in solutioneering101Quiz.questions" class="question-container">
-        <p><strong>{{ question.text }}</strong></p>
-        <div v-for="(answer, answerIndex) in question.answers">
-          <md-radio class="md-primary" :id="'answer-' + answer.id" v-model="userAnswers[questionIndex]" :name="question.id.toString()" :md-value="answer.id">{{ answer.text }}</md-radio>
+  <md-card class="max-width">
+    <md-card-header>
+      <md-card-header-text>
+        <div class="md-title">Solutioneering 101 Quiz</div>
+      </md-card-header-text>
+    </md-card-header>
+
+    <md-card-content>
+      <form novalidate @submit.stop.prevent="submit">
+        <div v-for="(question, questionIndex) in solutioneering101Quiz.questions" class="question-container">
+          <p><strong>{{ question.text }}</strong></p>
+          <div v-for="(answer, answerIndex) in question.answers">
+            <md-radio
+              class="md-primary"
+              :id="'answer-' + questionIndex + '-' + answerIndex"
+              :name="'question-' + questionIndex"
+              v-model="userAnswers[questionIndex]"
+              :md-value="answerIndex">
+                {{ answer }}
+                <div v-if="question.correctAnswerIndex === answerIndex && isSubmitted === true" class="correct-answer-label">
+                  <md-icon class="md-primary">done</md-icon>
+                  Correct Answer
+                </div>
+                <div v-if="userAnswers[questionIndex] === answerIndex && isSubmitted === true && answerIndex !== question.correctAnswerIndex" class="incorrect-answer-label">
+                  <md-icon class="md-warn">clear</md-icon>
+                  Incorrect Answer
+                </div>
+            </md-radio>
+            <div v-if="question.correctAnswerIndex === answerIndex && question.isCorrect === false" class="correct-answer-container">{{ question.correctAnswerReason }}</div>
+          </div>
         </div>
-      </div>
-    </form>
-    <md-layout md-align="end">
-      <md-button class="md-raised md-primary" @click.native.prevent="doContinue">Continue</md-button>
-    </md-layout>
-  </div>
+      </form>
+      <form-error v-if="errorMessage">{{ errorMessage }}</form-error>
+      <md-layout md-align="end">
+        <md-button class="md-raised md-primary" @click.native.prevent="doContinue">Continue</md-button>
+      </md-layout>
+    </md-card-content>
+  </md-card>
 </template>
 
 <script>
-  import { mapState } from 'vuex';
+  import { mapState, mapActions } from 'vuex';
+  import FormError from '../components/FormError.vue';
 
   export default {
     name: 'solutioneering-101-quiz',
+    components: {
+      FormError,
+    },
     data() {
       return {
-        userAnswers: [0, 0, 0],
+        isSubmitted: false,
+        userAnswers: [-1, -1, -1, -1, -1, -1, -1],
+        errorMessage: '',
       };
     },
     methods: {
       doContinue() {
-        this.$router.push({ name: 'dashboard' });
+        // Mark the form as isSubmitted
+        this.isSubmitted = true;
+
+        // Check the answers
+        var allQuestionsCorrect = true;
+        for (var questionIndex = 0; questionIndex < this.solutioneering101Quiz.questions.length; questionIndex++) {
+          var question = this.solutioneering101Quiz.questions[questionIndex];
+          var userAnswer = this.userAnswers[questionIndex];
+          var correctAnswer = question.correctAnswerIndex;
+
+          if (userAnswer === correctAnswer) {
+            this.updateSolutioneering101QuizQuestionIsCorrectState({ questionIndex: questionIndex, isCorrect: true });
+          } else {
+            this.updateSolutioneering101QuizQuestionIsCorrectState({ questionIndex: questionIndex, isCorrect: false });
+            allQuestionsCorrect = false;
+          }
+        }
+
+        if (allQuestionsCorrect) {
+          this.updateSolutioneering101QuizCompletedStatus(true);
+          this.$router.push({ name: 'dashboard' });
+        } else {
+          this.errorMessage = 'Please correct your answers and select Continue';
+        }
       },
+      ...mapActions('onboarding', ['updateSolutioneering101QuizQuestionIsCorrectState', 'updateSolutioneering101QuizCompletedStatus']),
     },
     computed: {
       ...mapState('onboarding', ['solutioneering101Quiz']),
@@ -39,5 +93,23 @@
 <style  type="text/scss">
   .question-container {
     margin-bottom: 40px;
+  }
+
+  .correct-answer-container {
+    color: #4caf50;
+    margin-left: 28px;
+  }
+
+  .correct-answer-label,
+  .incorrect-answer-label {
+    display: inline-block;
+    margin-left: 10px;
+  }
+  .correct-answer-label {
+    color: #4caf50;
+  }
+
+  .incorrect-answer-label {
+    color: #ff5722;
   }
 </style>
