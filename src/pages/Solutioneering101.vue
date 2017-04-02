06db42c35843e2b6e883536c92ed2002cdb96b4e
Josh Grauer <jaggedaz@gmail.com>
2017-04-01 18:52:56 -0700

    Finished out the Solutioneering 101 Quiz page. (#2)

--- src/pages/Solutioneering101.vue
+++ src/pages/Solutioneering101.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <h1 class="md-title">Solutioneering 101</h1>
-    <p>Review the presentation below and select Continue to take a short quiz.</p>
-    <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="100%" height="360" src="https://prezi.com/embed/tvcaqouutano/?bgcolor=ffffff&amp;lock_to_path=1&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE1leUxmMzFkTE00VmV0bW9yQ2h2VG4xT21Sb2c3VzZNZHhOSCtISjlYZz0&amp;landing_sign=71YgME8qEh4WbYhAMB6LlBqe8B6zk_q3wgVM8Wg7JZg"></iframe>
-    <md-layout md-align="end">
-      <md-button class="md-raised md-primary" @click.native.prevent="doContinue" style="margin-top: 20px">Continue</md-button>
-    </md-layout>
-  </div>
+  <md-card class="max-width">
+    <md-card-header>
+      <md-card-header-text>
+        <div class="md-title">Solutioneering 101</div>
+      </md-card-header-text>
+    </md-card-header>
+
+    <md-card-content>
+      <p>Review the presentation below and select Continue to take a short quiz.</p>
+      <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="100%" height="360" src="https://prezi.com/embed/tvcaqouutano/?bgcolor=ffffff&amp;lock_to_path=1&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE1leUxmMzFkTE00VmV0bW9yQ2h2VG4xT21Sb2c3VzZNZHhOSCtISjlYZz0&amp;landing_sign=71YgME8qEh4WbYhAMB6LlBqe8B6zk_q3wgVM8Wg7JZg"></iframe>
+      <md-layout md-align="end">
+        <md-button class="md-raised md-primary" @click.native.prevent="doContinue" style="margin-top: 20px">Continue</md-button>
+      </md-layout>
+    </md-card-content>
+  </md-card>
 </template>
 
 <script>
--- src/store/onboarding.js
+++ src/store/onboarding.js
@@ -24,67 +24,90 @@ export default {
       completed: false,
       questions: [
         {
-          id: 1,
-          text: '1.  Question 1.',
-          correctAnswerIndex: 0,
-          correctAnswerReason: '',
+          text: 'What does VMOST stand for?',
+          correctAnswerIndex: 3,
+          correctAnswerReason: 'The VMOST communicates the project plan so it can be in alignment from a general to specific level.',
           answers: [
-            {
-              id: 1,
-              text: 'Answer 1',
-            },
-            {
-              id: 2,
-              text: 'Answer 2',
-            },
-            {
-              id: 3,
-              text: 'Answer 3',
-            },
+            'A. Very Massive Obstacles Some Take',
+            'B. Value Money Or Stakeholder Tears',
+            'C. Vision Mission Objectives Strategies Tactics',
+            'D. Vomit Makes Oranges Smell Tasty',
           ],
+          isCorrect: undefined,
         },
         {
-          id: 2,
-          text: '2.  Question 2.',
-          correctAnswerIndex: 0,
-          correctAnswerReason: '',
+          text: 'What are the Project Assets?',
+          correctAnswerIndex: 4,
+          correctAnswerReason: 'All of these assets are necessary to completing a successful GreenLight project! Utilize the Chapter Toolkit for templates, examples, and more.',
           answers: [
-            {
-              id: 4,
-              text: 'Answer 1',
-            },
-            {
-              id: 5,
-              text: 'Answer 2',
-            },
-            {
-              id: 6,
-              text: 'Answer 3',
-            },
+            'A. Memorandum of Understanding (MOU)',
+            'B. Project Schedule/Timeline',
+            'C. Project Report',
+            'D. Green Paper',
+            'E. All of the above',
+            'F. None of the above',
           ],
+          isCorrect: undefined,
         },
         {
-          id: 3,
-          text: '3.  Question 3.',
+          text: 'What is the Mastermind?',
           correctAnswerIndex: 0,
-          correctAnswerReason: '',
+          correctAnswerReason: 'The GreenLight MasterMind events exist to help Solutioneers share their ideas with students from other projects, troubleshoot barriers they may face, and ultimately help them on their path to successfully completing their sustainability project.',
           answers: [
-            {
-              id: 7,
-              text: 'Answer 1',
-            },
-            {
-              id: 8,
-              text: 'Answer 2',
-            },
-            {
-              id: 9,
-              text: 'Answer 3',
-            },
+            'A. Monthly meeting for project teams to cross-collaborate',
+            'B. The Big Boss at the end of the project',
+            'C. The report identifying key points from the data',
+            'D. The folder containing all Project Assets',
           ],
+          isCorrect: undefined,
+        },
+        {
+          text: 'What is the Laugh and Learn?',
+          correctAnswerIndex: 3,
+          correctAnswerReason: 'Personal success and friendships require more than just project work experience. The Laugh and Learns are designed to bridge Solutioneers project from GreenLight to infinity and beyond.',
+          answers: [
+            'A. Monthly meeting for Solutioneers to acquire exclusive professional development skills',
+            'B. Monthly event for Solutioneers to just have fun',
+            'C. Dance pose when you spill your drink at the club',
+            'D. A and B',
+          ],
+          isCorrect: undefined,
+        },
+        {
+          text: 'What is the Solutioneer Showcase?',
+          correctAnswerIndex: 3,
+          correctAnswerReason: 'Our Solutioneer Showcase is our public event where students have the opportunity to share their great work with the community; Showcases include student project presentations, Q&A, professional networking, great food, and more.',
+          answers: [
+            'A. Professional networking event every academic term',
+            'B. The presentation of Solutioneer projects',
+            'C. Dance pose after you take a shot of wheat grass',
+            'D. A and B',
+          ],
+          isCorrect: undefined,
+        },
+        {
+          text: 'True or False: Solutioneers can expect to spend about 5 hours/week doing GreenLight work (this includes team meetings, individual work, and social events)',
+          correctAnswerIndex: 0,
+          correctAnswerReason: 'Creating real solutions for real businesses requires real time commitment; all Solutioneers are expected to commit at least 5 hours each week to GreenLight-related work',
+          answers: [
+            'A. True',
+            'B. False',
+          ],
+          isCorrect: undefined,
+        },
+        {
+          text: 'Within how much time is a Solutioneer expected to respond to communications (at least acknowledging message received)?',
+          correctAnswerIndex: 1,
+          correctAnswerReason: 'GreenLight Solutions is a professional organization, and we expect our students to be professional with their communication; responding within a 48-hour window is generally accepted in the business world.',
+          answers: [
+            'A. Immediately',
+            'B. Within 48 hours',
+            'C. Within a week',
+            'D. Whenever they get around to it',
+          ],
+          isCorrect: undefined,
         },
       ],
-      userAnswers: [0, 0, 0],
     },
   },
   mutations: {
@@ -95,6 +118,12 @@ export default {
     setCommitmentAgreement(state, { name, date, signed }) {
       if (signed) Object.assign(state.commitmentAgreement, { signed });
       else Object.assign(state.commitmentAgreement, { name, date });
+    },
+    setSolutioneering101QuestionIsCorrectState(state, { questionIndex, isCorrect }) {
+      state.solutioneering101Quiz.questions[questionIndex].isCorrect = isCorrect;
+    },
+    setSolutioneering101QuizCompletedStatus(state, value) {
+      state.solutioneering101Quiz.completed = value;
     },
   },
   actions: {
@@ -136,6 +165,13 @@ export default {
       return getUserId()
       .then(userId => backand.object.update(USERS_OBJECT, userId, data))
       .then(() => true);
+    },
+    updateSolutioneering101QuizQuestionIsCorrectState({ commit }, value) {
+      commit('setSolutioneering101QuestionIsCorrectState', value);
+    },
+    updateSolutioneering101QuizCompletedStatus({ commit }, value) {
+      // TODO: Call in to backand to update Solutionering 101 quiz status
+      commit('setSolutioneering101QuizCompletedStatus', value);
     },
   },
 };
