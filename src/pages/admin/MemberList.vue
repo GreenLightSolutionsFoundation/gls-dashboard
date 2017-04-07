a890d82c9634772e26b1d1c8876d06e265aa6f34
Joe Fleming <joe.fleming@gmail.com>
2017-04-06 20:37:51 -0700

    tweak table layout

--- src/pages/admin/MemberList.vue
+++ src/pages/admin/MemberList.vue
@@ -19,28 +19,29 @@
           <!-- <md-table-head md-sort-by="active">Active</md-table-head> -->
           <md-table-head>Active</md-table-head>
           <md-table-head>Contact</md-table-head>
+          <md-table-head>Onboarded</md-table-head>
           <md-table-head>Chapter</md-table-head>
           <md-table-head>Role</md-table-head>
           <md-table-head>Started</md-table-head>
-          <md-table-head>Onboarded??</md-table-head>
         </md-table-row>
       </md-table-header>
 
       <md-table-body>
         <md-table-row v-for="member in pageMembers" :key="member.id">
           <md-table-cell>
-            <md-icon v-if="member.currentlyActive">check</md-icon>
+            <md-icon v-if="member.currentlyActive" @click.native="deactivate(member.id)">check_box</md-icon>
+            <md-icon v-if="!member.currentlyActive" @click.native="activate(member.id)">check_box_outline_blank</md-icon>
           </md-table-cell>
           <md-table-cell>
             <div>{{ member.fullName }}</div>
             <div>{{ member.email }}</div>
           </md-table-cell>
-          <md-table-cell>{{ member.position }}</md-table-cell>
-          <md-table-cell>{{ member.chapter }}</md-table-cell>
-          <md-table-cell>{{ member.semesterStarted }}</md-table-cell>
           <md-table-cell>
-            <md-icon v-if="member.onboarded">check</md-icon>
+            <md-icon v-if="member.onboarded">star</md-icon>
           </md-table-cell>
+          <md-table-cell>{{ member.chapter }}</md-table-cell>
+          <md-table-cell>{{ member.position }}</md-table-cell>
+          <md-table-cell>{{ member.semesterJoined }}</md-table-cell>
         </md-table-row>
         <md-table-row v-if="pageMembers.length < perPage" v-for="num in (perPage - pageMembers.length)" :key="'_empty'+num">
           <md-table-cell></md-table-cell>
