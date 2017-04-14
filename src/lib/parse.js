8661cec8287a55ee79c14796fe2d993ef4044c35
Joe Fleming <joe.fleming@gmail.com>
2017-04-13 20:17:14 -0700

    switch to using parse for user accounts

---
+++ src/lib/parse.js
@@ -0,0 +1,7 @@
+import parse from 'parse';
+
+parse.initialize('eb2c1139-b8bd-4f30-8c8c-8856debef042');
+// parse.javascriptKey = '4hsr5gHTorMGuZ3F4xN9WI3RfPJtJ08d';
+parse.serverURL = 'https://parse.buddy.com/parse';
+
+export default parse;
