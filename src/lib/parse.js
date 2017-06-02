ee88bab9960a360f3f008bcb94770f071f631070
Edward Gilmore <edgilmore@live.com>
2017-06-02 09:06:49 -0700

    Upgrade packages (#12)
    * Updated packages
    Updated parse application key
    
    * updated packages
    
    * removed global eslint backand reference

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -1,6 +1,6 @@
 import parse from 'parse';
 
-parse.initialize('eb2c1139-b8bd-4f30-8c8c-8856debef042');
+parse.initialize('697c8deb-a86f-4c4a-ba04-de72e295b295');
 // parse.javascriptKey = '4hsr5gHTorMGuZ3F4xN9WI3RfPJtJ08d';
 parse.serverURL = 'https://parse.buddy.com/parse';
 
