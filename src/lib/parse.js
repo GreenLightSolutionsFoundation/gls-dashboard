1879ed486353781c92fe2aa21134b6d725a5061e
Joe Fleming <w33ble@users.noreply.github.com>
2017-06-08 20:24:29 -0700

    Quick Tweaks (#15)
    * switch eslint to yml
    
    * remove unused js key from parse config

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -1,7 +1,6 @@
 import parse from 'parse';
 
 parse.initialize('697c8deb-a86f-4c4a-ba04-de72e295b295');
-// parse.javascriptKey = '4hsr5gHTorMGuZ3F4xN9WI3RfPJtJ08d';
 parse.serverURL = 'https://parse.buddy.com/parse';
 
 export default parse;
