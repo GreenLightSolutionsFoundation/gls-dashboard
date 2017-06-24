938cffa0a2038f91277f9f4294254a9c214d198e
Ed Gilmore <edgilmore@live.com>
2017-06-24 12:41:11 -0700

    added implementation for the admin navigation

--- src/components/admin/Navigation.vue
+++ src/components/admin/Navigation.vue
@@ -1,42 +1,23 @@
 <template>
     <md-tabs>
-        <md-tab id="movies" md-label="Movies">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-        </md-tab>
-    
-        <md-tab id="music" md-label="Music">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-        </md-tab>
-    
-        <md-tab id="books" md-label="Books">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-        </md-tab>
-    
-        <md-tab id="pictures" md-label="Pictures" md-tooltip="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-        </md-tab>
-    
-        <md-tab id=" playlists " md-label="Playlists ">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-        </md-tab>
-    
-        <md-tab id="albums " md-label="Albums ">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-        </md-tab>
-    
-        <md-tab id="settings " md-label="Settings ">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
-        </md-tab>
-    
-        <md-tab id="account " md-label="Account ">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        <md-tab id="{{area.name}}" md-label="{{area.label}}" v-for="area in areas" :key="area.id">
+            <router-link to="{{area.name}}">{{area.label}}</router-link>
         </md-tab>
     </md-tabs>
 </template>
 <script>
 export default {
     name: 'Navigation'
+    data() {
+        return {
+            areas: [
+                { name: 'members', id: 'Members', label: 'Members', },
+                { name: 'projects', id: 'Projects', label: 'Projects', },
+                { name: 'chapters', id: 'Chapters', label: 'Chapters', },
+                { name: 'partner organizations', id: 'PartnerOrganizations', label: 'Partner Organizations', }
+            ]
+        }
+    }
 }
 </script>
 
