748a16815fab1292a54334546f8aac9c4d9dae73
Joe Fleming <joe.fleming@gmail.com>
2017-06-24 11:50:03 -0700

    re-query on member sorting

--- src/pages/admin/MemberList.vue
+++ src/pages/admin/MemberList.vue
@@ -12,7 +12,7 @@
 
     <!-- TODO: add sorting -->
     <!-- <md-table md-sort="name" @sort="onSort"> -->
-    <md-table :md-sort="sortField" :md-sort-type="sortOrder" @sort="onSort">
+    <md-table :md-sort="filters.sortField" :md-sort-type="filters.sortOrder" @sort="onSort">
       <md-table-header>
         <md-table-row>
         <!-- TODO: add sorting -->
@@ -50,7 +50,7 @@
           <md-table-cell>{{ member.position }}</md-table-cell>
           <md-table-cell>{{ member.semesterJoined }}</md-table-cell>
         </md-table-row>
-        <md-table-row v-if="pageMembers.length < perPage" v-for="num in (perPage - pageMembers.length)" :key="'_empty'+num">
+        <md-table-row v-if="pageMembers.length < filters.perPage" v-for="num in (filters.perPage - pageMembers.length)" :key="'_empty'+num">
           <md-table-cell></md-table-cell>
           <md-table-cell></md-table-cell>
           <md-table-cell></md-table-cell>
@@ -66,7 +66,7 @@
       <md-button class="md-icon-button md-raised" @click.native="prevPage">
         <md-icon>arrow_back</md-icon>
       </md-button>
-      <div>Page {{ currentPage}} of {{ totalPages }} ({{ totalMembers }} members)</div>
+      <div>Page {{ filters.currentPage}} of {{ totalPages }} ({{ totalMembers }} members)</div>
       <md-button class="md-icon-button md-raised" @click.native="nextPage">
         <md-icon>arrow_forward</md-icon>
       </md-button>
@@ -76,47 +76,46 @@
 
 <script>
 import { mapState, mapActions } from 'vuex';
-import sortArray from 'sort-array';
 
 export default {
   name: 'admin-member-list-page',
-  data() {
-    return {
+  data: () => ({
+    filters: {
       perPage: 20,
       currentPage: 1,
       sortField: 'lastName',
       sortOrder: 'asc',
-    };
-  },
+    },
+  }),
   created() {
     // fetch member lost on creation
-    this.getMembers();
+    this.refreshMembers();
   },
   computed: {
-    ...mapState('admin/members', {
-      members(state) {
-        const sortedMembers = sortArray([...state.members], this.sortField);
-        return (this.sortOrder === 'asc') ? sortedMembers : sortedMembers.reverse();
-      },
-    }),
+    ...mapState('admin/members', ['members']),
     totalPages() {
-      return Math.max(1, Math.ceil(this.members.length / this.perPage));
+      return Math.max(1, Math.ceil(this.members.length / this.filters.perPage));
     },
     totalMembers() {
       return this.members.length;
     },
     pageMembers() {
       // make sure we never go beyond the last page
-      if (this.totalPages && this.currentPage > this.totalPages) this.currentPage = this.totalPages;
+      if (this.totalPages && this.filters.currentPage > this.totalPages) {
+        this.filters.currentPage = this.totalPages;
+      }
 
-      const start = (this.currentPage - 1) * this.perPage;
-      const end = start + this.perPage;
+      const start = (this.filters.currentPage - 1) * this.filters.perPage;
+      const end = start + this.filters.perPage;
 
       return this.members.slice(start, end);
     },
   },
   methods: {
-    ...mapActions('admin/members', ['getMembers', 'setActive']),
+    ...mapActions('admin/members', ['setActive', 'getMembers']),
+    refreshMembers() {
+      this.getMembers(this.filters);
+    },
     activate(member) {
       this.setActive({ member, active: true });
     },
@@ -124,18 +123,22 @@ export default {
       this.setActive({ member, active: false });
     },
     nextPage() {
-      this.currentPage = Math.min(this.totalPages, this.currentPage + 1);
+      this.filters.currentPage = Math.min(this.totalPages, this.filters.currentPage + 1);
+      this.refreshMembers();
     },
     prevPage() {
-      this.currentPage = Math.max(1, this.currentPage - 1);
+      this.filters.currentPage = Math.max(1, this.filters.currentPage - 1);
+      this.refreshMembers();
     },
     onPagination({ page, size }) {
-      this.perPage = size;
-      this.currentPage = page;
+      this.filters.perPage = size;
+      this.filters.currentPage = page;
+      this.refreshMembers();
     },
     onSort({ name, type }) {
-      this.sortField = name;
-      this.sortOrder = type;
+      this.filters.sortField = name;
+      this.filters.sortOrder = type;
+      this.refreshMembers();
     },
   },
 };
