5875a0806df99097db4ab0ca018b09f481c0a89f
Ed Gilmore <edgilmore@live.com>
2017-06-24 11:25:44 -0700

    Inital commit of navigation  component for admin

---
+++ src/components/admin/Navigation.vue
@@ -0,0 +1,46 @@
+<template>
+    <md-tabs>
+        <md-tab id="movies" md-label="Movies">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+        </md-tab>
+    
+        <md-tab id="music" md-label="Music">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+        </md-tab>
+    
+        <md-tab id="books" md-label="Books">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    
+        <md-tab id="pictures" md-label="Pictures" md-tooltip="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    
+        <md-tab id=" playlists " md-label="Playlists ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    
+        <md-tab id="albums " md-label="Albums ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    
+        <md-tab id="settings " md-label="Settings ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    
+        <md-tab id="account " md-label="Account ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+        </md-tab>
+    </md-tabs>
+</template>
+<script>
+export default {
+    name: 'Navigation'
+}
+</script>
+
+<style lang="sass">
+
+</style>
+
