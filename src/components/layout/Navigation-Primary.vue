3e8294448bc4809bd09f03bdacd491ed5047d1da
Ed Gilmore <edgilmore@live.com>
2017-06-24 16:07:44 -0700

    Changed the route for the admin button
    Conflicts:
    	src/components/layout/Navigation-Primary.vue

--- src/components/layout/Navigation-Primary.vue
+++ src/components/layout/Navigation-Primary.vue
@@ -1,27 +1,27 @@
 <template>
-  <md-theme md-name="white">
-    <md-whiteframe>
-      <md-toolbar>
-        <bulb class="navbar-bulb"></bulb>
-        <brand class="navbar-brand"></brand>
-        <md-button @click.native.prevent="navigateTo('dashboard')">Dashboard</md-button>
-        <md-button v-if="isAdmin" @click.native.prevent="navigateTo('admin-members')">Admin</md-button>
-        <md-menu md-align-trigger class="goto-right">
-          <md-button md-menu-trigger>
-            <span>Abbey Christansen</span>
-            <md-avatar>
-              <img src="https://placeimg.com/40/40/people/5" alt="People">
-            </md-avatar>
-          </md-button>
-          <md-menu-content class="solid-background">
-            <md-list-item>
-              <md-button @click.native.prevent="doLogout">Logout</md-button>
-            </md-list-item>
-          </md-menu-content>
-        </md-menu>
-      </md-toolbar>
-    </md-whiteframe>
-  </md-theme>
+    <md-theme md-name="white">
+        <md-whiteframe>
+            <md-toolbar>
+                <bulb class="navbar-bulb"></bulb>
+                <brand class="navbar-brand"></brand>
+                <md-button @click.native.prevent="navigateTo('dashboard')">Dashboard</md-button>
+                <md-button v-if="isAdmin" @click.native.prevent="navigateTo('admin')">Admin</md-button>
+                <md-menu md-align-trigger class="goto-right">
+                    <md-button md-menu-trigger>
+                        <span>Abbey Christansen</span>
+                        <md-avatar>
+                            <img src="https://placeimg.com/40/40/people/5" alt="People">
+                        </md-avatar>
+                    </md-button>
+                    <md-menu-content class="solid-background">
+                        <md-list-item>
+                            <md-button @click.native.prevent="doLogout">Logout</md-button>
+                        </md-list-item>
+                    </md-menu-content>
+                </md-menu>
+            </md-toolbar>
+        </md-whiteframe>
+    </md-theme>
 </template>
 
 <script>
