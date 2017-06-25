15e9af26210b87f2d5653ddb319e2be71f6ac4a5
Ed Gilmore <edgilmore@live.com>
2017-06-24 19:07:03 -0700

    Initial commit for projects list view

---
+++ src/pages/admin/ProjectList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-table v-once>
+      <md-table-header>
+        <md-table-row>
+          <md-table-head>Dessert (100g serving)</md-table-head>
+          <md-table-head md-numeric>Calories (g)</md-table-head>
+          <md-table-head md-numeric>Fat (g)</md-table-head>
+          <md-table-head md-numeric>Carbs (g)</md-table-head>
+          <md-table-head md-numeric>Protein (g)</md-table-head>
+        </md-table-row>
+      </md-table-header>
+  
+      <md-table-body>
+        <md-table-row v-for="(row, index) in 5" :key="index">
+          <md-table-cell>Dessert Name</md-table-cell>
+          <md-table-cell v-for="(col, index) in 4" :key="index" md-numeric>10</md-table-cell>
+        </md-table-row>
+      </md-table-body>
+    </md-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
