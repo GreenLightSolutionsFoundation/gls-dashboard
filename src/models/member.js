0b8deff038162c0f2e7c8c3ddad7077565c508be
Joe Fleming <joe.fleming@gmail.com>
2017-07-06 20:23:17 -0700

    create member model

---
+++ src/models/member.js
@@ -0,0 +1,31 @@
+import parse from '../lib/parse';
+import User from './user';
+import ParseObject from './parse_object';
+
+export default class Member extends ParseObject {
+  constructor(userDetails) {
+    const ParseMember = parse.Object.extend('Member');
+    super(userDetails || new ParseMember());
+    this.user = new User(userDetails && userDetails.attributes.user);
+  }
+
+  get semesterJoined() { return this.instance.get('semesterJoined'); }
+  set semesterJoined(val) { return this.instance.set('semesterJoined', val); }
+
+  get chapter() { return this.instance.get('chapter'); }
+  set chapter(val) { return this.instance.set('chapter', val); }
+
+  get position() { return this.instance.get('position'); }
+  set position(val) { return this.instance.set('position', val); }
+
+  get currentlyActive() { return this.instance.get('currentlyActive'); }
+  set currentlyActive(val) { return this.instance.set('currentlyActive', Boolean(val)); }
+
+  query() {
+    const query = new parse.Query(this.instance);
+    query.include('user');
+    return query;
+  }
+}
+
+export const wrapMember = member => new Member(member);
