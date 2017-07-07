22d71e29f3fee9f36f92b160e15cfe85f69d08f0
Joe Fleming <joe.fleming@gmail.com>
2017-07-06 19:26:28 -0700

    move common methods to parse object class

--- src/models/parse_object.js
+++ src/models/parse_object.js
@@ -1,4 +1,31 @@
+import parse from '../lib/parse';
+
 export default class ParseObject {
+  constructor(instance) {
+    this.instance = instance;
+  }
+
+  get id() { return this.instance.id; }
+  get createdAt() { return this.instance.createdAt; }
+  get updatedAt() { return this.instance.updatedAt; }
+
+  query() {
+    return new parse.Query(this.instance);
+  }
+
+  subscribe() {
+    const query = this.query();
+    return query.subscribe();
+  }
+
+  save() {
+    return this.instance.save();
+  }
+
+  destroy() {
+    return this.instance.destroy();
+  }
+
   toJSON() {
     const keys = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
 
