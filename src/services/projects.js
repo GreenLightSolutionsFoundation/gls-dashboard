6fa6a60e2079bb44bd8a476c5524a685dc87b40a
Ed Gilmore <edgilmore@live.com>
2017-07-06 19:32:27 -0700

    added the getall function to retrieve projects from parse

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,7 +1,9 @@
+import queryBuilder from '../lib/query_builder';
 import parse from '../lib/parse';
 
-// import project from '../models/project';
+import { wrapProject } from '../models/project';
 
-export default function getAll() {
-  return parse.Query();
+export default function getAll(filters = {}) {
+  const query = new parse.Query(parse.Project);
+  return queryBuilder(query, filters).find().then(projects => projects.map(wrapProject));
 }
