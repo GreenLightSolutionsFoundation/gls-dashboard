6bc403fa216de4514822ae4255903731f694a724
Joe Fleming <joe.fleming@gmail.com>
2017-07-20 18:44:25 -0700

    update project model for modified parse_object class

--- src/models/project.js
+++ src/models/project.js
@@ -4,37 +4,30 @@ import ParseObject from './parse_object';
 
 export default class Project extends ParseObject {
   constructor(project) {
-    super();
-    this.project = project || new parse.Project();
+    super(project || new parse.Project());
   }
 
-  get id() { return this.project.id; }
-  get createdAt() { return this.project.createdAt; }
-  get updatedAt() { return this.project.updatedAt; }
+  get name() { return this.instance.get('name'); }
+  set name(val) { return this.instance.set('name', val); }
 
-  get name() { return this.project.get('name'); }
-  set name(val) { return this.project.set('name', val); }
+  get description() { return this.instance.get('description'); }
+  set description(val) { return this.instance.set('description'); }
 
-  get description() { return this.project.get('description'); }
-  set description(val) { return this.project.set('description'); }
+  get startDate() { return this.instance.get('startDate'); }
+  set startDate(val) { return this.instance.set('startDate', moment(val).utc().toDate()); }
 
-  get startDate() { return this.project.get('startDate'); }
-  set startDate(val) { return this.project.set('startDate', moment(val).utc().toDate()); }
+  get endDate() { return this.instance.get('endDate'); }
+  set endDate(val) { return this.instance.set('endDate', moment(val).utc().toDate()); }
 
-  get endDate() { return this.project.get('endDate'); }
-  set endDate(val) { return this.project.set('endDate', moment(val).utc().toDate()); }
+  get status() { return this.instance.get('status'); }
+  set status(val) { return this.instance.set('status', val); }
 
-  get status() { return this.project.get('status'); }
-  set status(val) { return this.project.set('status', val); }
+  get chapter() { return this.instance.get('chapter'); }
+  set chapter(val) { return this.instance.set('chapter'); }
 
-  get chapter() { return this.project.get('chapter'); }
-  set chapter(val) { return this.project.set('chapter'); }
+  get partnerOrganization() { return this.instance.get('partnerOrganization'); }
+  set partnerOrganization(val) { return this.instance.set('partnerOrganization', val); }
 
-  get partnerOrganization() { return this.project.get('partnerOrganization'); }
-  set partnerOrganization(val) { return this.project.set('partnerOrganization', val); }
-
-  get totalPositions() { return this.project.get('totalPositions'); }
-  set totalPositions(val) { return this.project.set('totalPositions', parseInt(val, 10)); }
-
-  save() { return this.project.save(); }
+  get totalPositions() { return this.instance.get('totalPositions'); }
+  set totalPositions(val) { return this.instance.set('totalPositions', parseInt(val, 10)); }
 }
