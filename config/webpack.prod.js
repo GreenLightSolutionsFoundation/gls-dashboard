de509a856e6253214dbc42d9bedf70b9c28bfb18
Joe Fleming <joe.fleming@gmail.com>
2017-07-21 16:38:55 -0700

    add script to run the built app

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ const baseConfig = require('./webpack.base');
 const ROOT = path.resolve(__dirname, '..');
 const srcPath = 'src';
 const distPath = 'public';
-const basepath = `/public/${process.env.BUDDY_PARSE_APP_ID}`;
+const basepath = process.env.APP_BASEPATH || `/public/${process.env.BUDDY_PARSE_APP_ID}`;
 
 module.exports = merge(baseConfig({ distPath }), {
   output: {
