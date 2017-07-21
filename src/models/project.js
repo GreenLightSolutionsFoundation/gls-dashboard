0fd80934ae1ac71ebe5cbf0561e53aa377ae38c2
Ed Gilmore <edgilmore@live.com>
2017-07-21 11:47:55 -0700

    setup the project constructor to fetch the model from parse

--- src/models/project.js
+++ src/models/project.js
@@ -4,7 +4,8 @@ import ParseObject from './parse_object';
 
 export default class Project extends ParseObject {
   constructor(project) {
-    super(project || new parse.Project());
+    const ParseProject = parse.Object.extend('Project');
+    super(project || new ParseProject());
   }
 
   get name() { return this.instance.get('name'); }
