fc0557f5a3ff47b64c2ee86bbe046828a13d955b
Ed Gilmore <edgilmore@live.com>
2017-07-21 18:40:57 -0700

    added return statement to get the projects object populated properly

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,14 +1,14 @@
-import { queryBuilder } from '../lib/query_builder';
+import queryBuilder from '../lib/query_builder';
 import Project, { wrapProject } from '../models/project';
 
 export function getAll(filters = {}) { // eslint-disable-line import/prefer-default-export
   const project = new Project();
   const query = project.query();
   const options = {
-    include: ['chapter'],
+    include: ['chapter', 'partnerOrganization', 'projectLead'],
   };
   Object.assign(filters, options);
   return queryBuilder(query, filters).find().then((projects) => {
-    projects.map(wrapProject);
+    return projects.map(wrapProject);
   });
 }
