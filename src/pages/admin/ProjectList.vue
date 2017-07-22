26ecef804a957b4ff79e058c6676a17ab7bd5858
Ed Gilmore <edgilmore@live.com>
2017-07-22 10:43:57 -0700

    updated the projects layout

--- src/pages/admin/ProjectList.vue
+++ src/pages/admin/ProjectList.vue
@@ -3,18 +3,17 @@
     <md-table v-once>
       <md-table-header>
         <md-table-row>
-          <md-table-head>Dessert (100g serving)</md-table-head>
-          <md-table-head md-numeric>Calories (g)</md-table-head>
-          <md-table-head md-numeric>Fat (g)</md-table-head>
-          <md-table-head md-numeric>Carbs (g)</md-table-head>
-          <md-table-head md-numeric>Protein (g)</md-table-head>
+          <md-table-head v-for="(column, index) in columns" :key="index">{{column.name}}</md-table-head>
         </md-table-row>
       </md-table-header>
   
       <md-table-body>
-        <md-table-row v-for="(row, index) in 5" :key="index">
-          <md-table-cell>Dessert Name</md-table-cell>
-          <md-table-cell v-for="(col, index) in 4" :key="index" md-numeric>10</md-table-cell>
+        <md-table-row v-for="(project, index) in projects" :key="index">
+          <md-table-cell>{{project.name}}</md-table-cell>
+          <md-table-cell>{{project.startDate}}</md-table-cell>
+          <md-table-cell>{{project.endDate}}</md-table-cell>
+          <md-table-cell>{{project.totalPositions}}</md-table-cell>
+          <md-table-cell>{{project.description}}</md-table-cell>
         </md-table-row>
       </md-table-body>
     </md-table>
@@ -26,18 +25,25 @@ import { mapActions } from 'vuex';
 export default {
   data() {
     return {
-      projects: [],
+      projects: {},
+      columns: [],
     };
   },
   methods: {
     ...mapActions('admin/projects', ['getProjects']),
     getColumns: () => {
-      const columns = [];
+      const columns = [{ name: 'test1' }, { name: 'test2' }, { name: 'test3' }, { name: 'test4' }, { name: 'test5' }];
       return columns;
     },
   },
   created() {
     this.projects = this.getProjects();
+    this.columns = this.getColumns();
+  },
+  computed: {
+    updateColumns: function(){
+      return this.projects._result;
+    }
   },
 };
 </script>
