8b30184f58bfee0349a9212b9539c8da498366b4
Ed Gilmore <edgilmore@live.com>
2017-07-27 20:15:50 -0700

    fixed the admin navigation to properly highlight the active tab
    fixed naming convention on project selection service and component reference

--- src/components/admin/Navigation-Admin.vue
+++ src/components/admin/Navigation-Admin.vue
@@ -1,53 +1,54 @@
 
 <template>
   <md-tabs md-centered @change="tabChange">
-    <md-tab
-      v-for="area in areas"
-      :id="area.id"
-      :md-label="area.label"
-      :md-active="activeTab && activeTab.routeName === area.routeName"
-      :key="area.id"
-    />
+    <md-tab v-for="area in areas" :id="area.id" :md-label="area.label" :md-active="activeTab && activeTab.routeName === area.routeName" :key="area.id" />
   </md-tabs>
 </template>
 <script>
-  export default {
-    name: 'Navigation-Admin',
-    data: () => ({
-      firstLoad: true,
-      activeTab: null,
-      areas: [
-        { routeName: 'admin-members', id: 'Members', label: 'Members' },
-        { routeName: 'admin-projects', id: 'Projects', label: 'Projects' },
-        //{ routeName: 'admin-chapters', id: 'Chapters', label: 'Chapters' },
-        //{ routeName: 'admin-organizations',
-            //id: 'PartnerOrganizations',
-            //label: 'Partner Organizations'
-        //},
-      ],
-    }),
-    created() {
-      // track the active tab so we can set it correctly, because vue-material is broken
-      this.activeTab = this.areas.find(area => area.routeName === this.$route.name);
+export default {
+  name: 'Navigation-Admin',
+  data: () => ({
+    firstLoad: true,
+    activeTab: null,
+    areas: [
+      { routeName: 'admin-members', id: 'Members', label: 'Members' },
+      { routeName: 'admin-projects', id: 'Projects', label: 'Projects' },
+      //{ routeName: 'admin-chapters', id: 'Chapters', label: 'Chapters' },
+      //{ routeName: 'admin-organizations',
+      //id: 'PartnerOrganizations',
+      //label: 'Partner Organizations'
+      //},
+    ],
+  }),
+  watch: {
+    $route: function (value) {
+      this.setActiveTab(value);
     },
-    methods: {
-      tabChange(index) {
-        // ignore the first tab change, which happens on tabs mount, because vue-material is broken
-        if (!this.firstLoad) {
-          const selectedArea = this.areas[index];
+  },
+  created(){
+    this.setActiveTab(this.$route);
+  },
+  methods: {
+    tabChange(index) {
+      // ignore the first tab change, which happens on tabs mount, because vue-material is broken
+      if (!this.firstLoad) {
+        const selectedArea = this.areas[index];
 
-          if (!selectedArea || !this.activeTab) return;
+        if (!selectedArea || !this.activeTab) return;
 
-          if (selectedArea.routeName !== this.activeTab.routeName) {
-            this.activeTab = selectedArea;
-            this.$router.push({ name: selectedArea.routeName });
-          }
+        if (selectedArea.routeName !== this.activeTab.routeName) {
+          this.activeTab = selectedArea;
+          this.$router.push({ name: selectedArea.routeName });
         }
+      }
 
-        this.firstLoad = false;
-      },
+      this.firstLoad = false;
     },
-  };
+    setActiveTab (value){
+      this.activeTab = this.areas.find(area => area.routeName === value.name);
+    },
+  },
+};
 </script>
 
 <style lang="sass">
