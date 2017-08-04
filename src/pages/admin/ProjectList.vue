018d03bd042726fd7c50540f86bc3cb3545e6f9f
Ed Gilmore <edgilmore@live.com>
2017-08-03 20:03:42 -0700

    updated the layout and added the search icon

--- src/pages/admin/ProjectList.vue
+++ src/pages/admin/ProjectList.vue
@@ -5,13 +5,24 @@
         <md-toolbar class="md-transparent">
           <md-input-container>
             <md-layout md-flex="50">
-              <h2 class="md-title">Projects</h2>
+              <md-layout md-flex="75">
+                <h2 class="md-title">Projects</h2>
+              </md-layout>
+              <md-layout md-flex="25">
+                <md-button class="md-icon-button">
+                  <md-icon>assignment</md-icon>
+                </md-button>
+              </md-layout>
             </md-layout>
             <md-layout md-flex="50">
-              <md-button class="md-icon-button">
-                <md-icon>assignment</md-icon>
-              </md-button>
-              <md-input placeholder="search"></md-input>
+              <md-layout md-flex="25">
+                <md-button class="md-icon-button">
+                  <md-icon>search</md-icon>
+                </md-button>
+              </md-layout>
+              <md-layout md-flex="75">
+                <md-input placeholder="search"></md-input>
+              </md-layout>
             </md-layout>
           </md-input-container>
         </md-toolbar>
