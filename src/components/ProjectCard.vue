7f2297c7eedd6590d8e8b267d413a3f029680c06
Joe Fleming <joe.fleming@gmail.com>
2017-08-03 19:23:54 -0700

    remove local state from project cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,14 +1,14 @@
 <template>
   <md-card class="project-card">
 
-    <md-card-area class="project-card-header" v-if="logo">
-      <img :src="logo" :alt="projectName">
+    <md-card-area class="project-card-header" v-if="project.logo">
+      <img :src="project.logo" :alt="project.projectName">
     </md-card-area>
 
     <md-card-area>
       <md-card-header>
-        <div class="md-title">{{projectName}}</div>
-        <div class="md-subhead">{{partnerName}}</div>
+        <div class="md-title">{{project.projectName}}</div>
+        <div class="md-subhead">{{project.partnerName}}</div>
       </md-card-header>
 
       <md-card-content>
@@ -18,17 +18,19 @@
               <md-icon>people</md-icon>
               <span
                 class="project-card-content-summary-item-description"
-                v-bind:style="{ color: openPositions !== 0 ? 'auto' : 'red' }">
-                {{positionsStatusMessage}}
+                v-bind:style="{ color: project.openPositions !== 0 ? 'auto' : 'red' }">
+                {{project.positionsStatusMessage}}
               </span>
             </div>
             <div class="project-card-content-summary-item">
               <md-icon>date_range</md-icon>
-              <span class="project-card-content-summary-item-description">{{projectStartDate | formatDate}} - {{projectEndDate | formatDate}}</span>
+              <span class="project-card-content-summary-item-description">
+                {{project.projectStartDate | formatDate}} - {{project.projectEndDate | formatDate}}
+              </span>
             </div>
           </div>
 
-          <div>{{projectDescription}}</div>
+          <div>{{project.projectDescription}}</div>
         </div>
       </md-card-content>
     </md-card-area>
@@ -37,26 +39,30 @@
       <div class="pick-option-text">My Pick #</div>
 
       <div @click="handleSelect">
+        wtf
         <md-button-toggle md-single class="md-primary">
           <md-button
             class="md-icon-button"
-            :class="{'md-toggle': project.isRank1Selected}"
             value="1"
-            :disabled="project.openPositions !== 0 ? false : true">
+            :class="{ 'md-toggle': project.isRank1Selected }"
+            :disabled="project.openPositions !== 0 ? false : true"
+          >
             1
           </md-button>
           <md-button
             class="md-icon-button"
-            :class="{'md-toggle': project.isRank2Selected}"
+            :class="{ 'md-toggle': project.isRank2Selected }"
             value="2"
-            :disabled="project.openPositions !== 0 ? false : true">
+            :disabled="project.openPositions !== 0 ? false : true"
+          >
             2
           </md-button>
           <md-button
             class="md-icon-button"
-            :class="{'md-toggle': project.isRank3Selected}"
+            :class="{ 'md-toggle': project.isRank3Selected }"
             value="3"
-            :disabled="project.openPositions !== 0 ? false : true">
+            :disabled="project.openPositions !== 0 ? false : true"
+          >
             3
           </md-button>
         </md-button-toggle>
@@ -67,16 +73,11 @@
 
 <script>
   import moment from 'moment';
-  
+
   export default {
     name: 'project-card',
     props: {
       project: Object,
-    },
-    data() {
-      const rank = this.rank || 0;
-      const project = this.project || {};
-      return { rank, ...project };
     },
     computed: {
       positionsStatusMessage() {
@@ -90,8 +91,9 @@
     },
     methods: {
       handleSelect(ev) {
-        this.rank = ev.target.value || this.rank;
-        this.$emit('rank-selected', { selectedRank: parseInt(this.rank, 10), projectId: this.projectId });
+        console.log('handleSelect', rank, project.id)
+        // this.rank = ev.target.value || this.rank;
+        // this.$emit('rank-selected', { selectedRank: parseInt(this.rank, 10), projectId: this.projectId });
       },
     },
     filters: {
@@ -107,7 +109,7 @@
   width: 100%;
   max-width: 340px;
   margin-bottom: 40px;
-  
+
   .project-card-header {
     display: flex;
 
@@ -125,7 +127,7 @@
 
       .project-card-content-summary-item {
         margin-bottom: 5px;
-        
+
         .project-card-content-summary-item-description {
           vertical-align: bottom;
           padding-left: 5px;
