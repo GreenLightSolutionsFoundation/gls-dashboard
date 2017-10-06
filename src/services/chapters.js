1cdf656e86a332f2544ba05963b19b9182a071cf
Edward Gilmore <edgilmore@live.com>
2017-10-05 19:12:32 -0700

    added support to for creating users with chapters

---
+++ src/services/chapters.js
@@ -0,0 +1,9 @@
+import queryBuilder from '../lib/query_builder';
+import Chapter from '../models/chapter';
+
+// eslint-disable-next-line
+export function getAll(filters = {}) {
+  const chapter = new Chapter();
+  return queryBuilder(chapter.query(), filters).find()
+    .then(results => results.map(result => new Chapter(result)));
+}
