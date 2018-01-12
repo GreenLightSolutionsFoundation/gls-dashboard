f11b284450a7f1d0767fa247602f66113b87ed47
Edward Gilmore <edgilmore@live.com>
2018-01-11 19:59:57 -0700

    removed md-layout components: no longer supported in the material vuejs

--- src/components/layout/Navigation-Primary.vue
+++ src/components/layout/Navigation-Primary.vue
@@ -1,72 +1,68 @@
 <template>
-    <md-theme md-name="white">
-        <md-whiteframe>
-            <md-toolbar>
-                <bulb class="navbar-bulb"></bulb>
-                <brand class="navbar-brand"></brand>
-                <md-button @click.native.prevent="navigateTo('dashboard')">Dashboard</md-button>
-                <md-button v-if="isAdmin" @click.native.prevent="navigateTo('admin')">Admin</md-button>
-                <md-menu md-align-trigger class="goto-right">
-                    <md-button md-menu-trigger>
-                        <span>{{ this.user.fullName }}</span>
-                        <md-avatar>
-                            <img :src="this.user.gravatar" alt="People">
-                        </md-avatar>
-                    </md-button>
-                    <md-menu-content class="solid-background">
-                        <md-list-item>
-                            <md-button @click.native.prevent="doLogout">Logout</md-button>
-                        </md-list-item>
-                    </md-menu-content>
-                </md-menu>
-            </md-toolbar>
-        </md-whiteframe>
-    </md-theme>
+  <md-toolbar>
+    <bulb class="navbar-bulb"></bulb>
+    <brand class="navbar-brand"></brand>
+    <md-button @click.native.prevent="navigateTo('dashboard')">Dashboard</md-button>
+    <md-button v-if="isAdmin" @click.native.prevent="navigateTo('admin')">Admin</md-button>
+    <md-menu md-align-trigger class="goto-right">
+        <md-button md-menu-trigger>
+            <span>{{ this.user.fullName }}</span>
+            <md-avatar>
+                <img :src="this.user.gravatar" alt="People">
+            </md-avatar>
+        </md-button>
+        <md-menu-content class="solid-background">
+            <md-list-item>
+                <md-button @click.native.prevent="doLogout">Logout</md-button>
+            </md-list-item>
+        </md-menu-content>
+    </md-menu>
+  </md-toolbar>
 </template>
 
 <script>
-import { mapActions, mapGetters, mapState } from 'vuex';
-import bulb from './Bulb.vue';
-import brand from './Brand.vue';
+import { mapActions, mapGetters, mapState } from "vuex";
+import bulb from "./Bulb.vue";
+import brand from "./Brand.vue";
 
 export default {
-  name: 'Navigation-Primary',
+  name: "Navigation-Primary",
   components: {
     bulb,
-    brand,
+    brand
   },
   computed: {
-    ...mapGetters('authentication', ['isAdmin']),
-    ...mapState('authentication', ['user']),
+    ...mapGetters("authentication", ["isAdmin"]),
+    ...mapState("authentication", ["user"])
   },
   methods: {
-    ...mapActions('authentication', ['logout']),
+    ...mapActions("authentication", ["logout"]),
     navigateTo(routeName) {
       this.$router.push({ name: routeName });
     },
     doLogout() {
       this.logout().then(() => {
-        this.$router.push({ name: 'login' });
+        this.$router.push({ name: "login" });
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .navbar-bulb {
-    width: 50px;
+  width: 50px;
 }
 
 .navbar-brand {
-    width: 110px;
+  width: 110px;
 }
 
 .solid-background {
-    background-color: #FDFDFD;
+  background-color: #fdfdfd;
 }
 
 .goto-right {
-    margin-left: auto;
+  margin-left: auto;
 }
 </style>
