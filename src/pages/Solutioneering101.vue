ff30c141b171deed85860f11d120187aea7df06c
Edward Gilmore <edgilmore@live.com>
2018-02-02 11:11:27 -0700

    removed the deprecated md-layout element

--- src/pages/Solutioneering101.vue
+++ src/pages/Solutioneering101.vue
@@ -9,9 +9,7 @@
     <md-card-content>
       <p>Review the presentation below and select Continue to take a short quiz.</p>
       <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="100%" height="360" src="https://prezi.com/embed/tvcaqouutano/?bgcolor=ffffff&amp;lock_to_path=1&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE1leUxmMzFkTE00VmV0bW9yQ2h2VG4xT21Sb2c3VzZNZHhOSCtISjlYZz0&amp;landing_sign=71YgME8qEh4WbYhAMB6LlBqe8B6zk_q3wgVM8Wg7JZg"></iframe>
-      <md-layout md-align="end">
-        <md-button class="md-raised md-primary" @click.native.prevent="doContinue" style="margin-top: 20px">Continue</md-button>
-      </md-layout>
+      <md-button class="md-raised md-primary" @click.native.prevent="doContinue" style="margin-top: 20px">Continue</md-button>
     </md-card-content>
   </md-card>
 </template>
