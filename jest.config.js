6e662831b12ffb2bd75cd9d824a0cc9801296267
Edward Gilmore <edgilmore@live.com>
2018-05-24 20:40:51 -0700

    refactor: update testing pattern

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,11 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.(js|jsx)?$': 'babel-jest',
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: ['<rootDir>/(tests/unit/**/*.spec.(ts|tsx|js)|**/__tests__/*.(ts|tsx|js))'],
+  testMatch: ['<rootDir>/(tests/unit/**/*.spec.(ts|tsx|js)|**/tests/*.(ts|tsx|js))'],
 };
