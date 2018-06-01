ff569584932e3f693ec7690fb5032ca1af3017e6
Edward Gilmore <edgilmore@live.com>
2018-05-31 20:21:02 -0700

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ['@vue/app'],
+  presets: [
+    '@vue/app',
+    [
+      'env',
+      {
+        targets: {
+          node: '6.11',
+          browsers: ['> 5% in US', 'last 2 versions'],
+        },
+        modules: false,
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ['transform-es2015-modules-commonjs'],
+    },
+  },
 };
