ac0082b141975182dc720cdab91b53fe9583d976
Edward Gilmore <edgilmore@live.com>
2018-05-31 19:18:50 -0700

    refactor: import vue and vue router for test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,7 +1,10 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import { shallowMount } from '@vue/test-utils';
 import App from '../../src/App.vue';
 
 describe('App.vue', () => {
+  Vue.use(VueRouter);
   it('renders app', () => {
     const wrapper = shallowMount(App);
     expect(wrapper).toBeDefined();
