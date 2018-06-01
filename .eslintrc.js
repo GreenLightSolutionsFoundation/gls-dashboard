aa6fa96ae95ba2b96774632fd9b7e051672340aa
Edward Gilmore <edgilmore@live.com>
2018-05-31 20:02:22 -0700

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
   root: true,
-  extends: [
-    'airbnb-base',
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ['airbnb-base', 'plugin:vue/essential', '@vue/prettier'],
   rules: {
-    'prettier/prettier': ['error']
+    'prettier/prettier': ['error'],
   },
   env: {
-    "test": {
-      "presets": ["es2015", "stage-0"]
-    }
-  }
-}
+    jest: true,
+  },
+};
