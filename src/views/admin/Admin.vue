7dd754d21d2390e62339a39e146024d738213319
Ed Gilmore <edgilmore@live.com>
2018-12-20 20:44:13 -0700

    refactor: moved the pages into a views folder

---
+++ src/views/admin/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AdminNavigation></AdminNavigation>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import PrimaryNavigation from '../../components/layout/Navigation-Primary.vue';
+  import AdminNavigation from '../../components/admin/Navigation-Admin.vue';
+
+  export default {
+    components: {
+      AdminNavigation,
+      PrimaryNavigation,
+    },
+  };
+</script>
